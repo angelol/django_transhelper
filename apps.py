c97aedb475490922af6b37b3fb31c48b31a3659e
Angelo Laub <angelolaub@gmail.com>
2023-08-22 20:39:30 +0200

    name bs

--- apps.py
+++ apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TranshelperConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django_transhelper"
+    name = "django-transhelper"
