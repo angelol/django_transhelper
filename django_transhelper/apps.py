ad9d8ace50f40457b3abc9c739dbd7b4261a260d
Angelo Laub <angelolaub@gmail.com>
2023-08-22 20:44:20 +0200

    rename

--- django_transhelper/apps.py
+++ django_transhelper/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TranshelperConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django-transhelper"
+    name = "django_transhelper"
